3eafa89e2dbdc1e336d1d643d30c2ae1986b014c
Emelie <emelor@gmail.com>
2013-02-13 17:01:21 +0100

    AdSome beauty fixes

--- raspberryserver.go
+++ raspberryserver.go
@@ -11,9 +11,7 @@ Web server lets user configure the system through web form
 */
 
 import (
-	//"encoding/json"
 	"fmt"
-	//"sync"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -26,6 +24,11 @@ var rainLimit = 4.0
 var rain = false
 var rainStruct = &rainCheck{}
 var weatherUrl = "http://www.yr.no/stad/Sverige/Stockholm/Stockholm/varsel.xml"
+type configuration struct{
+	moist float64
+	rain float64
+	url string
+}
 
 
 //Web server:
@@ -64,19 +67,19 @@ func handle(w http.ResponseWriter, r *http.Request) {
 		}
 	fmt.Fprintln(w, "<form>")
 	fmt.Fprintln(w, "Input lowest acceptable soil moisture level: <br/>")
-	fmt.Fprintf(w, "moisture (min: 0.0, max: 1.0): <input type='text' name='moisture' value='%v'> <br/>", minMoisture)
+	fmt.Fprintf(w, "moisture (min: 0.0, max: 1.0): <input type='text' name='moisture' value='%v'> <br/><br/>", minMoisture)
 
 	fmt.Fprintln(w, "How many millimeters of rain in the forecast are required to delay watering? <br/>")
-	fmt.Fprintf(w, "Rain (mm): <input type='text' name='rainlimit' value='%v'> <br/>", rainLimit)
+	fmt.Fprintf(w, "Rain (mm): <input type='text' name='rainlimit' value='%v'> <br/><br/>", rainLimit)
 
 	fmt.Fprintln(w, "Input weather source URL for your location from <a href='http://fil.nrk.no/yr/viktigestader/verda.txt'>yr.no</a>: <br/>")
-	fmt.Fprintf(w, "URL: <input type='text' name='URL' value='%v' size=%v> <br/>", weatherUrl, len(weatherUrl))
+	fmt.Fprintf(w, "URL: <input type='text' name='URL' value='%v' size=%v> <br/><br/>", weatherUrl, len(weatherUrl))
 
 	fmt.Fprintln(w, "Total rain forecast in your location during the next 24 hours: ")
 	fmt.Fprintln(w, rainStruct.rainTotal)
-	fmt.Fprintln(w, " mm </br>" )
+	fmt.Fprintln(w, " mm </br><br/>" )
 	//put in override "water anyway? buttons: today, always, no"
-	//insert minimum mm of rain per 24 h for delayed watering
+	
 	fmt.Fprintln(w, "<input type='submit' value='Submit'>")
 	fmt.Fprintln(w, "</form>")
 }
